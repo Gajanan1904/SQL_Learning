42f0e301966239389d849b058795575cf23124b8
Gajanan1904 <152101821+Gajanan1904@users.noreply.github.com>
2026-02-05 23:32:11 +0530

    SQLLITE

---
+++ sqllite_basics.py
@@ -0,0 +1,31 @@
+import sqlite3
+#connect() create or opens database
+# connection=sqlite3.connect("school.db")
+# print("Database connected")
+# connection.close()
+
+# connection1=sqlite3.connect("school.db")
+# #cursor()-->run sql
+# cur=connection1.cursor()
+# cur.execute("""Create Table studentts( id INTEGER,name TEXT,marks INTEGER)""")
+# print("Table created successfully")
+# connection1.commit()
+# connection1.close()
+
+# connection1=sqlite3.connect("school.db")
+# cur=connection1.cursor()
+# cur.execute("""Insert into studentts values (1,"amit",90),(2,"Gajanan",100)""")
+# print("values insserted ")
+# connection1.commit()
+# connection1.close()
+
+# connection1=sqlite3.connect("school.db")
+# cur=connection1.cursor()
+# cur.execute("""select * from studentts""")
+
+# for row in cur.fetchall():
+#     print(row)
+# connection1.commit()
+# connection1.close()
+
+            
